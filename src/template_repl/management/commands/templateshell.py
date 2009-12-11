90bd1aac2d13ec31f7771142bbbdb03d02e3a5d0
Cody Soyland <codysoyland@gmail.com>
2009-12-11 00:59:32 -0600

    This adds a --pdb option to the management command which bypasses the template repl and instead starts pdb (or ipdb) with the template context merged into the namespace.

--- src/template_repl/management/commands/templateshell.py
+++ src/template_repl/management/commands/templateshell.py
@@ -9,10 +9,11 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-u', '--url', dest='url', help='Preload context from given URL (just the path, such as "/admin/").', default=None),
         make_option('-c', '--context', dest='context', help='Supply context as dictionary. Note: This gets evaled.', default=None),
+        make_option('--pdb', dest='use_pdb', action='store_true', help='Use the template context provided by -u or -c in a pdb shell instead of a template shell.', default=False),
     )
     help = 'Shell to interact with the template language. Context can be loaded by passing a URL with -u.'
 
-    def handle(self, url, context, *args, **kwargs):
+    def handle(self, url, context, use_pdb, *args, **kwargs):
         if context:
             context_dict = eval(context)
         else:
@@ -30,5 +31,20 @@ class Command(BaseCommand):
                 elif isinstance(response.context, ContextList):
                     # TODO: probably should try to merge all contexts
                     context = response.context[0]
-        setup_readline_history()
-        run_shell(context)
+        if use_pdb:
+            pdb_with_context(context)
+        else:
+            setup_readline_history()
+            run_shell(context)
+
+def pdb_with_context(context):
+    vars = []
+    for context_dict in context.dicts:
+        for k, v in context_dict.items():
+            vars.append(k)
+            locals()[k] = v
+    try:
+        import ipdb as pdb
+    except ImportError:
+        import pdb
+    pdb.set_trace()
